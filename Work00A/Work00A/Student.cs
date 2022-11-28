7c1d4fb31b5534e5b3a4dc0b854a42bfc35ee406
Vandaler <panuwat.tha@kkumail.com>
2022-11-28 16:52:01 +0700

    Work Update

---
+++ Work00A/Work00A/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Work00A
+{
+    class Student
+    {
+        public string name;
+        public float weight;
+    }
+}
